bc83cd8610bb7f14b86de949c1514e8675e098e2
kbyunghoon <dev.kbhoon@gmail.com>
2025-07-08 12:26:49 +0900

    feat: 포인트 조회 API 구현 (Controller 연결)

--- src/main/kotlin/io/hhplus/tdd/point/PointController.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/point")
-class PointController {
+class PointController(private val pointService: PointService) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     /**
@@ -16,7 +16,7 @@ class PointController {
     fun point(
         @PathVariable id: Long,
     ): UserPoint {
-        return UserPoint(0, 0, 0)
+        return pointService.getPoint(id)
     }
 
     /**
