30caf619ffc0d596bcc243fdcf02b970a9ccfe64
kbyunghoon <dev.kbhoon@gmail.com>
2025-07-08 12:06:53 +0900

    test: 포인트 조회 실패하는 테스트 작성
    존재하지 않는 사용자 → 0포인트 반환
    기존 포인트가 있는 사용자 → 정확한 포인트 반환

---
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -0,0 +1,16 @@
+package io.hhplus.tdd.point
+
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.database.UserPointTable
+import org.springframework.stereotype.Service
+
+@Service
+class PointService(
+    private val userPointTable: UserPointTable,
+    private val pointHistoryTable: PointHistoryTable
+) {
+
+    fun getPoint(userId: Long): UserPoint {
+        TODO("포인트 조회")
+    }
+}
